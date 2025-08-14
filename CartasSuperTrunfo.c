deddc78d9827eff28790f3e5a6e8540368bc66ad
Groger-dev <gustavormsousa01@gmail.com>
2025-08-14 01:06:10 +0000

    Codificacao do programa

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -7,6 +7,36 @@
 //Teste larissa
 
 int main() {
+    char estado_A[10], estado_B[10], cidade1[10], cidade2[10];
+    int populacao1, populacao2, turismo1, turismo2;
+    float area1, area2, PIB1, PIB2;
+    
+    //Primeira carta
+    printf("Insira o nome do estado: ");
+    scanf("%s\n", estado_A);
+
+    printf("Insira o nome de uma cidade pertencente a %s: ", estado_A);
+    scanf("%s\n", cidade1);
+
+    printf("Qual é a população de %s?", cidade1);
+    scanf("%d\n", &populacao1);
+
+    printf("Qual é a área de %s em km²? ", cidade1);
+    scanf("%f\n", &area1);
+
+    printf("Qual é o PIB de %s? ", cidade1);
+    scanf("%f\n", &PIB1);
+
+    //Impressão da primeira carta
+    printf("Carta A01 cadastrada com sucesso!");
+
+    printf("Estado: %s\n", estado_A);
+    printf("Cidade: %s\n", cidade1);
+    printf("População de %s: %d\n", cidade1, populacao1);
+    printf("Área: %.2f\n", area1);
+    printf("PIB: %.2f\n", PIB1);
+
+        
     // Sugestão: Defina variáveis separadas para cada atributo da cidade.
     // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
     
